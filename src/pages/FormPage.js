baf31620fbb34cc7821a4494fdfbe5efd7ac50db
dranixxxx <phoang0302@gmail.com>
2020-12-07 14:16:23 +0700

    b

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FormPage = props => {
   /* eslint-disable jsx-a11y/anchor-has-content */
   return (
-    <a href="http://hadtech-elearning.tk" target="_blank" rel="noopener noreferrer" {...props} />
+    <a href="https://vcp1.hadtech.vn/" target="_blank" rel="noopener noreferrer" {...props} />
   );
 };
 
